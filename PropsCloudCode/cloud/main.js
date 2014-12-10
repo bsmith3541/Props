65d6fe3dc6370ccebfb49d02f55fb2d315bf47c1
Brandon Smith <brandon.smith@yale.edu>
2014-12-09 23:17:43 -0600

    Add Parse CloudeCode script to validate the uniquness of users against their Facebook IDs

--- PropsCloudCode/cloud/main.js
+++ PropsCloudCode/cloud/main.js
@@ -4,3 +4,27 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+var PropsUser = Parse.Object.extend("PropsUser");
+
+// Check if fbId is set, and enforce uniqueness based on the fbId column.
+Parse.Cloud.beforeSave("PropsUser", function(request, response) {
+  if (!request.object.get("fbId")) {
+    response.error('A PropsUser must have a fbId.');
+  } else {
+    var query = new Parse.Query(PropsUser);
+    query.equalTo("fbId", request.object.get("fbId"));
+    query.first({
+      success: function(object) {
+        if (object) {
+          response.error("A PropsUser with this Facebook ID already exists.");
+        } else {
+          response.success();
+        }
+      },
+      error: function(error) {
+        response.error("Could not validate uniqueness for this Props User.");
+      }
+    });
+  }
+});
